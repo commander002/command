2c476187cf6927111dd43b9ff086ba5d636a75ef
commander <111735942+commander002@users.noreply.github.com>
2023-07-02 03:37:29 -0700

    settings.js

--- settings.js
+++ settings.js
@@ -31,24 +31,24 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.botname = "C-BOT" //namabot kalian
+global.ownername = "𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
 global.email = "mdsandaru2@gmail.com" //bebas
 global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.premium = ['17865837776'] //premium user
+global.owner = ["31687886969", "17865837776"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "31687886969" // nomor wa kalian
+global.ownernomerr = 31687886969" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Md' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '+92' //sticker wm ubah
+global.author = '𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
